e7069f655ed8fc8cf70e5282b264048f952b5b97
PavelVlasenko <vlasenko-pav@yandex.ru>
2015-07-21 17:21:32 +0700

    commit

--- src/CreateUserAccouts.java
+++ src/CreateUserAccouts.java
@@ -1,14 +1,14 @@
 import java.io.*;
+import java.util.Set;
 import java.util.TreeSet;
 
 public class CreateUserAccouts
 {
-    private static TreeSet<Student> students;
-
+    private static Set<Student> studentsSet = new TreeSet<Student>();
 
     public static void main(String[] args) {
         //read file
-        File file = new File("/home/pvlasenko/Documets/Upwork/test.txt");
+        File file = new File("/home/pvlasenko/Documents/Upwork/test.txt");
         BufferedReader br = null;
 
         try
@@ -21,24 +21,24 @@ public class CreateUserAccouts
 
             while((str = br.readLine()) != null)
             {
-                if(rowCount == 5)
+                if(rowCount == 6)
                 {
+                    studentsSet.add(student);
                     student = new Student();
+                    rowCount = 0;
                 }
                 switch (rowCount)
                 {
                     case 0: student.studentNumber = str; break;
                     case 1: String[] arrayNames = str.split(", ");
-                            student.familyName = arrayNames[0];
-                            student.givenNames = arrayNames[1];
+                            student.familyName = arrayNames[0].trim();
+                            student.givenNames = arrayNames[1].trim();
                             break;
-                    case 4: student.birthday = str.split(" ");
-                    default: rowCount++;
-
+                    case 4: student.birthday = str.split(" ");break;
                 }
-
-
+                rowCount++;
             }
+            studentsSet.add(student);
         }
         catch(FileNotFoundException e)
         {
@@ -49,8 +49,20 @@ public class CreateUserAccouts
             e.printStackTrace();
         }
 
+        for (Student st : studentsSet)
+        {
+            st.calculatePassword();
+        }
+    }
+
+    private static void calculateUserName(TreeSet<Student> set)
+    {
+        username
+        for()
 
     }
+
+
 
 }
 
@@ -63,11 +75,12 @@ class Student implements Comparable<Student>
     String userName;
     String password;
 
-    public void calculateUsernamePassword()
+    public void calculatePassword()
     {
-        //calculateUsername
-
+        String month = birthday[1].length() > 3 ? birthday[1].substring(0, 3) : birthday[1];
+        password =  month + birthday[0] + studentNumber.substring(4);
     }
+
 
     @Override
     public int compareTo(Student o)
